4ad682578950f25d5a7f7c4f6b97c0722394b297
smpark <smpark7723@gmail.com>
2024-06-09 13:02:14 +0900

    Update : vercel 배포 설정

--- src/index.js
+++ src/index.js
@@ -3,32 +3,26 @@ const app = require('./configs/express')(express);
 const { stream } = require('./configs/winston');
 const morgan = require('morgan');
 const cors = require('cors');
-
 const path = require('path');
 const api = require('./api');
-
 const jwtMiddleware = require('./middleware/globalMiddleware/jwtMid');
 const limitMiddleware = require('./middleware/globalMiddleware/limiterMid');
 const errHandlerMiddleware = require('./middleware/globalMiddleware/errorHandlerMid');
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('combined', { stream }));
-app.use(jwtMiddleware); // 검증 미들웨어가 먼저 사용되어야 함.
-app.use(limitMiddleware); // api 접속이 단시간 급격하게 증가할 시 접속 제한
+app.use(jwtMiddleware);
+app.use(limitMiddleware);
+
 const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 app.use(api);
-
 app.use(errHandlerMiddleware);
 
-const port = process.env.PORT || 4001;
-app.listen(port, () => {
-  console.log(`Server Connected, ${port} port!`);
-});
+module.exports = app;
